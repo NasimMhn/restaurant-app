2c3233ca895ff439aff4df771b4d3cd110a61d79
nasimmhn <nasim.mahzoun@gmail.com>
2019-11-06 00:01:28 +0100

    Added a fetch to get city name

--- index.js
+++ index.js
@@ -2,25 +2,51 @@ const apiKey = "0d2ae46a0d3314ac244b0da1e2d88315";
 
 const cityId = 61;
 const cuisieId = 156;
-const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&cuisines=${cuisieId}`;
+const searchUrl = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&cuisines=${cuisieId}`;
+const cityUrl = `https://developers.zomato.com/api/v2.1/cities?city_ids=${cityId}`
 
-fetch(url, {
+// Fetching city information
+fetch(cityUrl, {
         headers: {
             "user-key": apiKey
         }
     })
     .then(res => res.json())
     .then(json => {
-        // console.log(json)
+        console.log(json)
+        let city = json.location_suggestions[0].name
+        document.getElementById("city").innerText = city
+    });
+
+// Fetching results from search
+fetch(searchUrl, {
+        headers: {
+            "user-key": apiKey
+        }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log(json)
         json.restaurants.forEach(resto => {
             restaurant = resto.restaurant
             console.log(restaurant)
+
+            let name = restaurant.name
+            let thumb = restaurant.thumb
+            let address = restaurant.location.address
+            let openhours = restaurant.timings
+
+            let rating = restaurant.user_rating.aggregate_rating
+            let ratingColor = restaurant.user_rating.rating_color
+            let ratingText = restaurant.user_rating.rating_text
+
             document.getElementById("restaurants").innerHTML +=
                 `<div class="restaurant">
-                    <img src=${restaurant.thumb}>
-                    <h4> ${restaurant.name}</h4> 
-                    <p> Address: ${restaurant.location.address}</p>
-                    <p> Opening hours: ${restaurant.timings}</p>
+                    <img src=${thumb}>
+                    <h4>${name}</h4>
+                    <span style="background-color:#${ratingColor}">${rating} ${ratingText}</span>
+                    <p> Address: ${address}</p>
+                    <p> Opening hours: ${openhours}</p>
                 </div>`
 
         });
